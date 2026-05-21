2df3e79f55c0e05efd81f76b1581c400ea47011c
amanchaurasiya7565 <amanchaurasiya7565@gmail.com>
2026-05-21 09:30:51 +0530

    last commit

---
+++ React/firstreactwork/src/App.jsx
@@ -0,0 +1,27 @@
+import './App.css';
+function App (){
+  
+  return(
+<div >
+    <h1>My Favourite Nature Gallery</h1>
+    <div className="images">
+      <div class="box" >
+        <img id="image"img src="https://images.unsplash.com/photo-1439066615861-d1af74d74000"></img>
+        <p id="img">Water Fall</p>
+      </div>
+       <div class="box"  >
+        <img  id="image"img src="https://s.yimg.com/fz/api/res/1.2/aoR7y3zcKFdyrcixboN91w--~C/YXBwaWQ9c3JjaGRkO2ZpPWZpbGw7aD00MTI7cHhvZmY9NTA7cHlvZmY9MTAwO3E9ODA7c3M9MTt3PTM4OA--/https://i.pinimg.com/736x/2e/08/67/2e0867bd359218dccf73c304fc4b09e2.jpg"></img>
+        <p id="img">Mountain view</p>
+      </div>
+       <div class="box" >
+        <img id="image"img src="https://s.yimg.com/fz/api/res/1.2/hHwuTklCYBMBHAoQ.5Cpew--~C/YXBwaWQ9c3JjaGRkO2ZpPWZpbGw7aD00MTI7cHhvZmY9NTA7cHlvZmY9MTAwO3E9ODA7c3M9MTt3PTM4OA--/https://i.pinimg.com/736x/b4/be/1b/b4be1b158e2bf56ca1bcb2484f52a9b0.jpg"></img>
+        <p id="name">Green Forest</p>
+      </div>
+    </div>
+    
+  </div>
+  );
+    
+  
+}
+export default App;
--- app.js
+++ app.js
@@ -65,9 +65,10 @@
 // let numbers=[1,2,3,4,5];
 // console.log(numbers);
  
-// let random=[1,"apple"];
+// let random=[1,"apple",true,[11,2,3],{name:"Aman",age: 25}];
 // console.log(random);
-// let arrnew=[1,2,3];
+// console.log(typeofrandom);
+// // let arrnew=[1,2,3];
 // arrnew.push(4);
 // console.log(arrnew);
 
@@ -96,15 +97,33 @@
 // });
 // console.log(squareArr);
 
+
+// let newarray = [1,2,3,4,5];
+// let squaredArray = newarray.map((num)=>num*5);
+// console.log(squaredArray);
+
 // let evenArr =arr.filter(function(element){
 //     return element % 2 ===0;
 // });
  // console.log(evenArr);
+
+
+//  let evenNumbers = [1,2,3,4,5];
+// let filteredEvenNumbers = evenNumbers.
+// filter(num=>num % 2 === 0);
+// console.log(filteredEvenNumbers);
+
+
+
 // let arr=[1,2,3,4,5];
 // let sum =arr.reduce(function(accumulator,currentValue){
 //     return accumulator + currentValue;
 // },0);
 // console.log(sum);
+
+// let arr=[1,2,3,4,5];
+// let sum =arr.reduce((accumulator,currentValue)=> accumulator + currentValue,0);
+// console.log(sum); 
 
 // function greet(){
 //     console.log("Hello, Aman!");
@@ -122,13 +141,102 @@
 // let result =add(5,10);
 // console.log (result);
 
+// function add(a,b){
+//  return a+b;
+// }
+// console.log(add(2,3));
+// const multiplly=function(a,b){                                    error
+//     return(a*b);
+// }
+// console.log(multiply(3,5));
+// let sayhi=()=> console.log("hi!");
+// sayhi();
+
+
+
+// const mul=(a,b)=>(a*b);
+// console.log(mul(2,3));
+
+
+
 // let greet2 =function(name){
 //     console.log("Hello," + name + "!");
 // };
 // greet2("BOb");
 //tamplet  literals
-let name3 = "Charlie";
-let greeting =`hello,${name3}!`;
-console.log(greeting);
+// let name3 = "Charlie";
+// let greeting =`hello,${name3}!`;
+// console.log(greeting);
+
+
+// let students =[
+//    {name  : "Aman",marks: 85},
+//    {name:"Dipu",marks:90},
+//    {name:"vlkhefoi",marks: 32}
+// ];
+// let marks = students.map(student=>student.marks);
+// console.log(marks);
+// let names = students.map(student=>student.name);
+// console.log(names);
+// let topstudent =students.filter(student=>student.marks>80);
+// console.log(topstudent);
+
+
+// mixed object
+let mixedobject = {
+   name: "Aman",
+   age: 25,
+   hobbies:["reading","travaling","coding"],
+   address:{
+      streat : "123 main st",
+      city:"india",
+      country:"Usa"
+   },
+
+   greet : function(){
+      console.log("Hello!");
+      // arrow function in object
+      let  arrowGreet=()=>{
+         console.log("Hi");
+      }
+   }
+};
+console.log(mixedobject.name);
+console.log(mixedobject.age);
+console.log(mixedobject.hobbies);
+console.log(mixedobject.name.hobbies[0]);
+console.log(mixedobject.name.hobbies[1]);
+
+console.log(mixedobject.name.hobbies[2]);
+console.log(mixedobject.address);
+console.log(mixedobject.address.streat);
+console.log(mixedobject.address.city);
+console.log(mixedobject.address.country);
+mixedobject.address.greet();
+
+// object inside array
+let people =[
+   {name:"Aman",age:25,},
+   {name:"bob",age:15,},
+   {name:"naman",age:35,},
+];
+console.log(people[0]);
+console.log(people[1].name);
+console.log(people[2].age);
+
+// array inside object
+let company = {
+   name: "Tech crop",
+   employee:["Alice","BOb","Charlie"]
+};
+console.log(company.name);
+console.log(company.employee);
+console.log(company.employee[0]);
+console.log(company.employee[1]);
+console.log(company.employee[2]);
+
+   
+
+
 
 
